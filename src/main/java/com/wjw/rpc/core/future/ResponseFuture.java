0e0e5167bcb3f024b506e25fa9dcd01dc9d74650
wang.jianwen <wang.jianwen@immomo.com>
2020-10-13 14:47:27 +0800

    first commit

--- src/main/java/com/wjw/rpc/core/future/ResponseFuture.java
+++ src/main/java/com/wjw/rpc/core/future/ResponseFuture.java
@@ -1,10 +1,12 @@
 package com.wjw.rpc.core.future;
 
+import java.util.Map;
+
 /**
  * @description:
  * @author: wang.jianwen
  * @create: 2020-10-12 19:06
  **/
 public class ResponseFuture {
-
+//    private static final Map<String, >
 }
